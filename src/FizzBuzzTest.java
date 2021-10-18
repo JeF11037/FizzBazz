cb44ef693ff52ba5a0d507a7867e8c4a4f913439
Opilane TTHK <opilane@tthk.ee>
2021-10-18 13:10:39 +0300

    Add test for multiples of five

--- src/FizzBuzzTest.java
+++ src/FizzBuzzTest.java
@@ -15,5 +15,11 @@ public class FizzBuzzTest {
 		FizzBuzz fb = new FizzBuzz();
 		assertEquals("fizz", fb.fizzbuzz(3));
 	}
+	
+	@Test
+	public void mupltiplesOfFive() {
+		FizzBuzz fb = new FizzBuzz();
+		assertEquals("buzz", fb.fizzbuzz(5));
+	}
 
 }
