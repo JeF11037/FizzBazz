8fac065d84c34c7ca9bb5a1adeaf1e02f8aa03b4
Opilane TTHK <opilane@tthk.ee>
2021-10-25 13:21:07 +0300

    getNumbers() implementation

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -3,5 +3,12 @@ import org.apache.commons.lang3.ArrayUtils;
 
 public class FizzBuzz {
 		
-
+	public int[] getNumbers() {
+		int[] numbers = new int[100];
+		for (int tick = 0; tick < numbers.length; tick++) {
+			numbers[tick] = tick + 1;
+		}
+		return numbers;
+	}
+	
 }
